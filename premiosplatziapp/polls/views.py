2eaeb927c8bde278fcca44ae33dca8af5658b5be
Luis Marquez <luisjose0527@gmail.com>
2023-02-07 12:33:13 -0400

    definiendo las primeras vistas de la app

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World")
+
+#esta es una function based view
+def index(request): 
+    return HttpResponse("Estas en la pagina principal de Premios Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
